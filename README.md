21df28b8d47d91808557896a93777837bfe8fa65
Pierre Fritsch <fritsch@puzzle.ch>
2015-05-01 16:08:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,78 @@
+# Frontend Cubie
+
+## The little front-end companion
+
+Cubie is an HTML5 starter kit that includes: packages and tasks managers, a local server with browser synchronization, a beautiful auto-generated documentation, a styles pre-processor, files minification and images optimisation.
+
+## A collection of tools
+
+* Gulp
+* Bower
+* HTML5 Boilerplate
+* LibSass
+* Susy
+* Breakpoint Sass
+* Autoprefixer
+* Hologram
+* BrowserSync
+* And some more
+
+## Requirements
+
+You'll need to have the following items installed before continuing.
+
+  * [Ruby](https://www.ruby-lang.org/en/documentation/installation/): Use the installer provided on the website.
+  * [Node.js](http://nodejs.org): Use the installer provided on the NodeJS website.
+  * [Gulp](http://gruntjs.com/): Run `[sudo] npm install --global gulp`
+
+## Quickstart
+
+Open you terminal, go to the folder: `cd [your_path]/frontend-cubie/`
+And then install the dependencies:
+
+```
+npm install
+bower install
+```
+
+While you're working on your project, run:
+
+`gulp` to generate the pages
+
+And you're set!
+
+
+## Directory Structure
+
+* `src/`: The sources
+* `src/index.html`: The content of the homepage
+* `src/assets`: Images, fonts, icons...
+* `src/scripts`: Javascripts
+* `src/styles`: SASS files
+
+* `app/`: The generated pages (overwritten overtime your run gulp)
+* `app/styleguide`: The auto-generated documentation
+
+
+#TODO
+
+[] Template system (assemble? handlebars?)
+[] Base grid system?
+[x] HTML mininfied
+[X] Images optimization
+[X] SVG Icons 
+[] Browser caching (filerev)
+[] Tests (karma)
+
+
+### How to add an icon?
+
+* Start by creating or choosing a vector icon from the [Entypo library](http://www.entypo.com/)
+* Add the SVG-formatted icon in `src/assets/icons/`
+* Link your icon in the html:
+
+```
+<svg class="icon-{name_of_you_icon}"><use xlink:href="#icon-{name_of_you_icon}"></use></svg>
+```
+
+* Run `gulp` again
---
+++ gulpfile.js
@@ -0,0 +1,194 @@
+// Requirements
+// ------------------------
+var gulp = require('gulp'),
+
+  // Utils
+  rename = require('gulp-rename'),
+  header = require('gulp-header'),
+  inject = require('gulp-inject'),
+  
+  // Template
+  minifyHTML = require('gulp-minify-html'),
+  wiredep = require('wiredep').stream,
+  svgstore = require('gulp-svgstore'),
+  
+  // Images
+  imagemin = require('gulp-imagemin'),
+  pngquant = require('imagemin-pngquant'),
+  
+  // Scripts
+  jshint = require('gulp-jshint'),
+  stylish = require('jshint-stylish'),
+  uglify = require('gulp-uglify'),
+
+  // Styles
+  sass = require('gulp-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  sourcemaps = require('gulp-sourcemaps'),
+  minifyCSS = require('gulp-minify-css'),
+  
+  // Doc
+  hologram = require('gulp-hologram'),
+  
+  // Server
+  browserSync = require('browser-sync');
+
+
+// Settings
+// ------------------------
+var banner = [
+  '/*!\n' +
+  ' * <%= package.name %>\n' +
+  ' * <%= package.title %>\n' +
+  ' * <%= package.url %>\n' +
+  ' * @author <%= package.author %>\n' +
+  ' * @version <%= package.version %>\n' +
+  ' * Copyright ' + new Date().getFullYear() + '. <%= package.license %> licensed.\n' +
+  ' */',
+  '\n'
+].join('');
+
+var src = './src/',
+  dist = './app/',
+  package = require('./package.json'),
+  reload = browserSync.reload;
+
+
+// Gulp Tasks
+// ------------------------
+
+
+// COPY
+// Copy extra files like .htaccess, robots.txt
+gulp.task('copy', function () {
+  gulp.src(['./.htaccess', './robots.txt'])
+    .pipe(gulp.dest(dist));
+});
+
+
+// TEMPLATE
+// Bower css and scripts inject +  SVG Sprite inject
+gulp.task('template', function () {
+
+  var svgs = gulp
+    .src(src + 'assets/icons/*.svg')
+    .pipe(svgstore({ inlineSvg: true }));
+
+  function fileContents (filePath, file) {
+    return file.contents.toString();
+  }
+
+  return gulp.src(src + 'index.html')
+    .pipe(wiredep({
+      includeSelf: true
+    }))
+    .pipe(inject(svgs, { transform: fileContents }))
+    .pipe(minifyHTML({
+      conditionals: true,
+      spare: true
+    }))
+    .pipe(gulp.dest(dist))
+    .pipe(reload({
+      stream: true
+    }));
+});
+
+
+// IMAGES
+// Optimization
+gulp.task('images', function () {
+  return gulp.src(src + 'assets/images/*')
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{
+        removeViewBox: false
+      }],
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest(dist + 'assets/images/'))
+    .pipe(reload({
+      stream: true
+    }));
+});
+
+
+// SCRIPTS
+// JSHint, Uglify
+gulp.task('scripts', function () {
+  return gulp.src(src + 'scripts/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish))
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest(dist + 'scripts'))
+    .pipe(reload({
+      stream: true
+    }));
+});
+
+
+// STYLES
+// LibSass, Minified
+gulp.task('styles', function () {
+  return gulp.src(src + 'styles/{,*/}*.{scss,sass}')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(minifyCSS())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(header(banner, {
+      package: package
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(dist + 'styles'))
+    .pipe(reload({
+      stream: true
+    }));
+});
+
+
+// DOC
+// Hologram: http://www.wearecube.ch/maintaining-living-style-guides-with-hologram/
+gulp.task('doc', function () {
+  return gulp.src('hologram_config.yml')
+    .pipe(hologram())
+    .pipe(reload({
+      stream: true
+    }));
+});
+
+
+// SERVER
+// Browser Sync
+gulp.task('serve', function () {
+  return browserSync.init(null, {
+    server: {
+      baseDir: dist,
+      routes: {
+        "/bower_components": "bower_components"
+      }
+    }
+  });
+});
+
+
+// Gulp Default Task
+// ------------------------
+// Having watch within the task ensures that 'sass' has already ran before watching
+gulp.task('default', ['copy', 'template', 'images', 'scripts', 'styles', 'doc', 'serve'], function () {
+  gulp.watch(src + '{,*/}*.html', ['template']);
+  gulp.watch(src + 'assets/icons/*.svg', ['template']);
+  gulp.watch(src + 'scripts/*.js', ['scripts']);
+  gulp.watch(src + 'assets/images/*', ['images']);
+  gulp.watch(src + 'styles/{,*/}*.{scss,sass}', ['styles', 'doc']);
+  gulp.watch(src + 'styles/styleguide.md', ['doc']);
+});
